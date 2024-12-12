2dd400283d500818ac5b28e33ee696af25e6585d
Роман <x-sel@Rambler.ru>
2024-12-12 11:55:25 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#  "1st program"
+print("\033[32m1st program\033[33m")
+res1 = 9**0.5 * 5
+print(res1)
+
+#  "2nd program"
+print("\033[32m2nd program\033[33m")
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#  "3rd program"
+print("\033[32m3rd program\033[33m")
+res1 = 2 * 2 + 2
+res2 = 2 * (2 + 2)
+print(res1, res2)
+print(res1 == res2)
+
+#  "4th program"
+print("\033[32m4th program\033[33m")
+str1 = '123.456'
+str1 = int(float(str1) * 10) % 10
+print(str1)
